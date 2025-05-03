62a8342a4cd8ad573727e7fdd14e66a6ee46d957
CaLeamari <carol.leamari@gmail.com>
2025-05-03 13:02:46 -0300

    Corrigindo importação duplicada do Express

--- api.js
+++ api.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-const express = require('express');
+
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('API de Data e Hora');
+  res.json({
+    date: new Date().toLocaleString('pt-BR'),
+    status: 'API no Render funcionando!'
+  });
 });
 
 app.get('/datetime', (req, res) => {
@@ -15,8 +19,10 @@ app.get('/datetime', (req, res) => {
   });
 });
 
+// Porta dinâmica para o Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
+console.log("Iniciando a aplicação...");
